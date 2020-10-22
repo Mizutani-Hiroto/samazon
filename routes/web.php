ba2df804525dfe48ab2d58fcdb24125ecd51a349
Mizutani-Hiroto <mztnhrt0428@gmail.com>
2020-10-22 00:52:58 +0000

    register create

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
 
 Route::resource('products','OriductController');
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+if(env('APP_ENV') === 'local') {
+    URL::forceScheme('https');
+}
