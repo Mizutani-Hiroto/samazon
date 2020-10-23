325f3622af76fd5b7884fd1d0df93f2dc9d9b453
Mizutani-Hiroto <mztnhrt0428@gmail.com>
2020-10-23 00:23:48 +0000

    set footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,12 +23,17 @@
 </head>
 <body>
     <div id="app">
+        
         @component('components.header')
         @endcomponent
 
         <main class="py-4 mb-5">
             @yield('content')
         </main>
+        
+        @component('components.footer')
+        @endcomponent
+        
     </div>
 </body>
 </html>
