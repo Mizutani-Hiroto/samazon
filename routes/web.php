d09335930667cf27cbc6554d1452e91854cf9f4a
Mizutani-Hiroto <mztnhrt0428@gmail.com>
2020-11-03 01:05:37 +0000

    create purchesed

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Route::get('users/carts','CartController@index')->name('carts.index');
 
 Route::post('users/carts','CartController@store')->name('carts.store');
 
+Route::delete('users/carts','CartController@destroy')->name('carts.destroy');
+
 Route::get('users/mypage','UserController@mypage')->name('mypage');
 Route::get('users/mypage/edit','UserController@edit')->name('mypage.edit');
 Route::get('users/mypege/address/edit','UserController@edit_address')->name('mypage.edit_address');
